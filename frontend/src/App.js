2a30b5410b6e9be5b99bc440623a001cbda43183
Abhik Das <130923397+AO811@users.noreply.github.com>
2024-07-08 21:46:18 +0530

    c

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { HashRouter,Routes,Route } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+
 
 function App() {
   return (
