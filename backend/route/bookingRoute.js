205461147923537102e47f350674e5df05f1a807
Abhik Das <130923397+AO811@users.noreply.github.com>
2024-07-09 13:17:50 +0530

    c

--- backend/route/bookingRoute.js
+++ backend/route/bookingRoute.js
@@ -4,8 +4,8 @@ const bookingRoute = express.Router();
 
 const { createRoom, deleteRoom, getAllRooms, getRoomById, updateRoom } = require("../controller/booking");
 
-bookingRoute.post('/createRoom/:id',createRoom)  
-bookingRoute.put('/UpdateRoom/',updateRoom)
+bookingRoute.post('/createRoom/',createRoom)  
+bookingRoute.put('/UpdateRoom/:id',updateRoom)
 bookingRoute.delete('/DeleteRoom/',deleteRoom)
 bookingRoute.get("/GetRoom/",getRoomById)
 bookingRoute.get("/",getAllRooms)
